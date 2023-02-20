3887c153446e115b81b1e34e43b68de4e3cdebf1
Alexandr <alexandr-troshin>
2023-02-21 00:54:32 +0400

    Домашняя задание Задача №23

--- two/Program.cs
+++ two/Program.cs
@@ -5,23 +5,22 @@
 Console.Clear();
 
 
-int x1 = ReadInt("Введите координату X первой точки: ");
-int y1 = ReadInt("Введите координату Y первой точки: ");
-int z1 = ReadInt("Введите координату Z первой точки: ");
-int x2 = ReadInt("Введите координату X второй точки: ");
-int y2 = ReadInt("Введите координату Y второй точки: ");
-int z2 = ReadInt("Введите координату Z второй точки: ");
+Console.WriteLine("Введите координату X1 первой точки: ");
+int x1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату Y1 первой точки: ");
+int y1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату Z1 первой точки: ");
+int z1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату X2 второй точки: ");
+int x2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату Y2 второй точки: ");
+int y2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату Z2 второй точки: ");
+int z2 = int.Parse(Console.ReadLine()!);
 
 int A = x2 - x1;
 int B = y2 - y1;
 int C = z1 - z2;
 
-double length = Math.Sqrt(A * A + B * B + C * C);
-Console.WriteLine($"Длинна отрезка {length}");
-
-// Функция ввода сообщения
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
+double Distans = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine($"Длинна отрезка: {Distans:f2}");
