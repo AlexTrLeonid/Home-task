1b2571322ecea8235d417110981b66be728cee4d
Alexandr <alexandr-troshin>
2023-02-24 00:47:14 +0400

    Домашняя работа Задача №27

---
+++ six/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число : ");
+int N = int.Parse(Console.ReadLine()!);
+int sum = 0;
+while (N > 0)
+{
+    sum += N % 10;
+    sum += N / 10;
+    sum += N;
+}
+Console.WriteLine("Сумма всех цифр этого числа = ", sum);
