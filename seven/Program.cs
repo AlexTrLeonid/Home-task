228fa242da1390a94de9ee8993f6c7c0a369432f
Alexandr <alexandr-troshin>
2023-02-24 02:51:47 +0400

    Домашняя работа Задача №29

--- seven/Program.cs
+++ seven/Program.cs
@@ -1,15 +1,12 @@
-﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-// 6, 1, 33 -> [6, 1, 33]m);
-Console.Clear();
+﻿Console.Clear();
 int[] FillArray()
 {
-    int[] array = new int[8];
+    int[] array = new int[9];
     int length = array.Length;
     int index = 0;
     while (index < length)
     {
-        array[index] = new Random().Next(1, 9);
+        array[index] = new Random().Next(1, 10);
         index++;
     }
     return array;
