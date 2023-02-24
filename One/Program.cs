5fe0dc9ad83814c8d9864fdb40f9d5b5ac9ac929
Alexandr <alexandr-troshin>
2023-02-24 15:55:09 +0400

    Домашняя работа Задача №19(исправленная)

--- One/Program.cs
+++ One/Program.cs
@@ -5,26 +5,33 @@
 // 23432 -> да
 
 Console.Clear();
-Console.WriteLine("Веести пятизначное число: ");
-int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите пятизначное число: ");
+int number = 0;
+while (true)
+{
+     number = int.Parse(Console.ReadLine()!);
+     if (number < 10000 || number > 99999)
+     {
+        Console.WriteLine("Введите пятизначное число: ");
+     }
+     else 
+     {
+        break;
+     }
+
+}
 int number1 = 0;
 int number2 = 0;
 int number3 = 0;
 int number4 = 0;
 
-if (number >= 100000 || number < 10000)
-{
-    Console.WriteLine("Ведите число 10000 до 99999");
-}
 
-else if (number < 99999)
-{
     number1 = number / 10000;
     number2 = number % 10;
     number3 = number / 1000 % 10;
     number4 = number % 100 / 10;
 
-    if (number1 == number2 || number3 == number4)
+    if (number1 == number2 && number3 == number4)
     {
         Console.Write("Да");
     }
@@ -32,4 +39,4 @@ else if (number < 99999)
     {
         Console.Write("Нет");
     }
-}
+
